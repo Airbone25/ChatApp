6a2a211468127129d9b3900b67037c12b1c057b1
Keshav Mehra <keshavmehra2005@gmail.com>
2024-04-20 17:44:55 +0530

    Commited the Project

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const {createServer} = require('node:http');
+const {join} = require('node:path');
+const {Server} = require('socket.io')
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/',(req,res)=>{
+    res.sendFile(join(__dirname,'index.html'))
+});
+
+const user = {};
+
+io.on('connection',(socket)=>{
+    console.log("a user is connected");
+
+    socket.on('user-connected',(name)=>{
+        user[socket.id]=name;
+        io.emit('user-connected',name);
+    })
+
+    socket.on('send',(msg)=>{
+        io.emit('send',{message: msg, name: user[socket.id]});
+    })
+
+    socket.on('disconnect',(me)=>{
+        console.log("user is disconnected");
+    })
+})
+
+server.listen(5500,()=>{
+    console.log("Server is running at http://localhost:5500");
+})
